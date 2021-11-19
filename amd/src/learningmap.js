c41258f03017ebd6cbaf4ff64a658fe564d13a2e
Stefan Hanauska <stefan@hanauska.info>
2021-11-19 13:55:20 +0100

    Rename CSS classes, remove unneccessary code

--- amd/src/learningmap.js
+++ amd/src/learningmap.js
@@ -268,7 +268,7 @@ export const init = () => {
 
     function clickHandler(event) {
         hideContextMenu();
-        if (event.target.classList.contains('place')) {
+        if (event.target.classList.contains('learningmap-place')) {
             event.preventDefault();
             if (firstPlace === null) {
                 firstPlace = event.target.id;
@@ -410,11 +410,7 @@ export const init = () => {
 
     function processPlacestore() {
         let svg = document.getElementById('learningmap_svgmap');
-        // svg.setAttribute('width', placestore.width);
-        // svg.setAttribute('height', placestore.height);
         svg.setAttribute('viewBox', '0 0 ' + placestore.width + ' ' + placestore.height);
-        //let container = document.getElementById('learningmap-editor-map');
-        //container.setAttribute('style', 'height: ' + (placestore.height + 4) + 'px');
     }
 
 };
