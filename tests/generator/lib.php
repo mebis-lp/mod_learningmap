e726d9d6e487949ec8c086b064c4aaced1e64576
Stefan Hanauska <stefan.hanauska@altmuehlnet.de>
2021-11-28 18:29:42 +0100

    Modify test structure

--- tests/generator/lib.php
+++ tests/generator/lib.php
@@ -31,29 +31,10 @@ class mod_learningmap_generator extends testing_module_generator {
 
         $record = (array)$record + array(
             'name' => 'test map',
-            'intro' => '<svg id="learningmap-svgmap" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" version="1.1" baseProfile="full" width="800" height="600">
-            <style id="learningmap-svgstyle" type="text/css">
-            .place {
-                fill: red;
-                stroke-width: 3px;
-                stroke: white;
-                visibility: hidden;
-            }
-            .path {
-                stroke: white;
-                stroke-width: 3px;
-                visibility: hidden;
-            }
-            </style><g id="backgroundGroup">
-                <image x="0" y="0" width="800" height="600" class="learningmap-background-image" id="learningmap-background-image" xlink:href="@@PLUGINFILE@@/Beispiel%20Hintergrund.jpg"></image>
-            </g>
-            <g id="pathsGroup">
-            <line class="path" id="p0_1" x1="287" y1="510" x2="230" y2="385"></line><line class="path" id="p1_3" x1="230" y1="385" x2="340" y2="294"></line><line class="path" id="p1_2" x1="230" y1="385" x2="88" y2="341"></line></g>
-            <g id="placesGroup">
-            <a id="a0" xlink:href=""><circle class="place draggable" id="p0" cx="287" cy="510" r="10"></circle><title id="titlep0"></title></a><a id="a1" xlink:href=""><circle class="place draggable" id="p1" cx="230" cy="385" r="10"></circle><title id="titlep1"></title></a><a id="a2" xlink:href=""><circle class="place draggable selected2" id="p2" cx="88" cy="341" r="10"></circle><title id="titlep2"></title></a><a id="a3" xlink:href=""><circle class="place draggable" id="p3" cx="340" cy="294" r="10"></circle><title id="titlep3"></title></a></g>
-            </svg>',
+            'intro' => file_get_contents('./test.svg'),
             'introformat' => 1,
-            'placestore' => '{"id":4,"places":[{"id":"p0","linkId":"a0","linkedActivity":"1038"},{"id":"p1","linkId":"a1","linkedActivity":"1046"},{"id":"p2","linkId":"a2","linkedActivity":"1048"},{"id":"p3","linkId":"a3","linkedActivity":"1048"}],"paths":[{"id":"p0_1","fid":"p0","sid":"p1"},{"id":"p1_3","fid":"p1","sid":"p3"},{"id":"p1_2","fid":"p1","sid":"p2"}],"startingplaces":["p0"],"placecolor":"red","strokecolor":"white"}'
+            'placestore' => file_get_contents('./test.json'),
+            'completiontype' => 2
         );
 
         return parent::create_instance($record, (array)$options);
