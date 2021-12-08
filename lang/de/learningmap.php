7cfdb318118ab32bd150ce1ec50f6a3fe877ab96
Stefan Hanauska <stefan.hanauska@csg-in.de>
2021-12-08 11:29:50 +0100

    Sort language files

--- lang/de/learningmap.php
+++ lang/de/learningmap.php
@@ -25,45 +25,45 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['modulename'] = 'Lernlandkarte';
-$string['modulenameplural'] = 'Lernlandkarten';
-$string['modulename_help'] = 'Lernlandkarten erlauben es, die Aktivitäten in einem Kurs als durch Pfade verbundene Orte auf einer Landkarte darzustellen. Einzelne Orte sind Start-Orte und werden zu Beginn dargestellt. Andere Orte und Pfade werden angezeigt, sobald die Aktivitäten der verbundenen Orte abgeschlossen sind.';
-$string['pluginname'] = 'Lernlandkarte';
-$string['name'] = 'Titel der Lernlandkarte';
-$string['name_help'] = 'Der Titel der Lernlandkarte wird nur angezeigt, wenn "Karte auf Kursseite anzeigen" nicht aktiviert ist.';
-$string['pluginadministration'] = 'Lernlandkarte Administration';
+$string['advancedsettings'] = 'Erweiterte Einstellungen';
 $string['backgroundfile'] = 'Hintergrundbild';
 $string['backgroundfile_help'] = 'Diese Datei wird als Hintergrund für die Lernlandkarte verwendet.';
-$string['svgcode'] = 'SVG Code';
-$string['learningmap'] = 'Lernlandkarte';
+$string['completion_with_all_places'] = 'Alle Orte müssen erreicht werden';
+$string['completion_with_all_targets'] = 'Alle Ziel-Orte müssen erreicht werden';
+$string['completion_with_one_target'] = 'Ein Ziel-Ort muss erreicht werden';
+$string['completiondetail:all_places'] = 'Alle Orte erreichen';
+$string['completiondetail:all_targets'] = 'Alle Ziel-Orte erreichen';
+$string['completiondetail:one_target'] = 'Einen Ziel-Ort erreichen';
+$string['completiontype'] = 'Art des Aktivitätsabschlusses';
+$string['editorhelp'] = 'Wie verwende ich den Editor?';
 $string['editplace'] = 'Ort bearbeiten';
-$string['startingplace'] = 'Start-Ort';
-$string['targetplace'] = 'Ziel-Ort';
-$string['showdescription'] = 'Karte auf Kursseite anzeigen';
-$string['showdescription_help'] = 'Wenn diese Option aktiviert ist, wird die Lernlandkarte auf der Kursseite angezeigt. Ist sie deaktiviert, wird die Lernlandkarte auf einer separaten Seite dargestellt.';
-$string['intro'] = $string['learningmap'];
+$string['hidepaths'] = 'Pfade verbergen';
+$string['infolink'] = 'https://www.mebis.bayern.de/p/70989';
+$string['intro'] = 'Lernlandkarte';
 $string['intro_help'] = '<ul><li><b>Hinzufügen eines Ortes:</b> Doppelklick</li>
 <li><b>Hinzufügen eines Pfades:</b> Zwei Orte nacheinander anklicken</li>
 <li><b>Entfernen eines Ortes:</b> Doppelklick auf den Ort</li>
 <li><b>Entfernen eines Pfades:</b> Doppelklick auf den Pfad</li>
 <li><b>Eigenschaften des Ortes verändern:</b> Rechtsklick</li></ul>';
+$string['learningmap'] = 'Lernlandkarte';
 $string['learningmap:addinstance'] = 'Lernlandkarte hinzufügen';
 $string['learningmap:view'] = 'Lernlandkarte anzeigen';
-$string['completiontype'] = 'Art des Aktivitätsabschlusses';
+$string['modulename'] = 'Lernlandkarte';
+$string['modulename_help'] = 'Lernlandkarten erlauben es, die Aktivitäten in einem Kurs als durch Pfade verbundene Orte auf einer Landkarte darzustellen. Einzelne Orte sind Start-Orte und werden zu Beginn dargestellt. Andere Orte und Pfade werden angezeigt, sobald die Aktivitäten der verbundenen Orte abgeschlossen sind.';
+$string['modulenameplural'] = 'Lernlandkarten';
+$string['name'] = 'Titel der Lernlandkarte';
+$string['name_help'] = 'Der Titel der Lernlandkarte wird nur angezeigt, wenn "Karte auf Kursseite anzeigen" nicht aktiviert ist.';
 $string['nocompletion'] = 'Keine automatischer Abschluss über die Karte';
-$string['completion_with_one_target'] = 'Ein Ziel-Ort muss erreicht werden';
-$string['completion_with_all_targets'] = 'Alle Ziel-Orte müssen erreicht werden';
-$string['completion_with_all_places'] = 'Alle Orte müssen erreicht werden';
-$string['completiondetail:one_target'] = 'Einen Ziel-Ort erreichen';
-$string['completiondetail:all_targets'] = 'Alle Ziel-Orte erreichen';
-$string['completiondetail:all_places'] = 'Alle Orte erreichen';
-$string['privacy:metadata'] = '';
-$string['places'] = 'Orte';
-$string['visited'] = 'Besucht';
 $string['paths'] = 'Pfade';
-$string['hidepaths'] = 'Pfade verbergen';
-$string['usecheckmark'] = 'Haken für abgeschlossene Orte';
-$string['advancedsettings'] = 'Erweiterte Einstellungen';
-$string['editorhelp'] = 'Wie verwende ich den Editor?';
+$string['places'] = 'Orte';
+$string['pluginadministration'] = 'Lernlandkarte Administration';
+$string['pluginname'] = 'Lernlandkarte';
+$string['privacy:metadata'] = '';
+$string['showdescription'] = 'Karte auf Kursseite anzeigen';
+$string['showdescription_help'] = 'Wenn diese Option aktiviert ist, wird die Lernlandkarte auf der Kursseite angezeigt. Ist sie deaktiviert, wird die Lernlandkarte auf einer separaten Seite dargestellt.';
+$string['startingplace'] = 'Start-Ort';
+$string['svgcode'] = 'SVG Code';
+$string['targetplace'] = 'Ziel-Ort';
 $string['usecasehelp'] = 'Wie verwende ich Lernlandkarten?';
-$string['infolink'] = 'https://www.mebis.bayern.de/p/70989';
+$string['usecheckmark'] = 'Haken für abgeschlossene Orte';
+$string['visited'] = 'Besucht';
