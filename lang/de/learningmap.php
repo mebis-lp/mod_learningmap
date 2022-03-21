b0e006570ef3129925e2286b9400b32fce4f644e
Stefan Hanauska <stefan.hanauska@csg-in.de>
2022-03-21 19:22:49 +0100

    MBS-6063: Add german lang file

---
+++ lang/de/learningmap.php
@@ -0,0 +1,71 @@
+<?php
+// mod_learningmap - A moodle plugin for easy visualization of learning paths
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Language file for mod_learningmap
+ *
+ * @package     mod_learningmap
+ * @copyright   2021-2022, ISB Bayern
+ * @author      Stefan Hanauska <stefan.hanauska@csg-in.de>
+ * @license     https://www.gnu.org/licenses/agpl-3.0.html GNU AGPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['advancedsettings'] = 'Erweiterte Einstellungen';
+$string['backgroundfile'] = 'Hintergrundbild';
+$string['backgroundfile_help'] = 'Diese Datei wird als Hintergrund für die Lernlandkarte verwendet.';
+$string['completion_with_all_places'] = 'Alle Orte müssen erreicht werden';
+$string['completion_with_all_targets'] = 'Alle Ziel-Orte müssen erreicht werden';
+$string['completion_with_one_target'] = 'Ein Ziel-Ort muss erreicht werden';
+$string['completiondetail:all_places'] = 'Alle Orte erreichen';
+$string['completiondetail:all_targets'] = 'Alle Ziel-Orte erreichen';
+$string['completiondetail:one_target'] = 'Einen Ziel-Ort erreichen';
+$string['completiondisabled'] = 'Die Abschlussverfolgung ist in den Kurseinstellungen deaktiviert. Ohne Abschlussverfolgung funktioniert dieses Plugin nicht.';
+$string['completiontype'] = 'Art des Aktivitätsabschlusses';
+$string['editorhelp'] = 'Wie verwende ich den Editor?';
+$string['editplace'] = 'Ort bearbeiten';
+$string['hidepaths'] = 'Pfade verbergen';
+$string['intro'] = 'Lernlandkarte';
+$string['intro_help'] = '<ul><li><b>Hinzufügen eines Ortes:</b> Doppelklick</li>
+<li><b>Hinzufügen eines Pfades:</b> Zwei Orte nacheinander anklicken</li>
+<li><b>Entfernen eines Ortes:</b> Doppelklick auf den Ort</li>
+<li><b>Entfernen eines Pfades:</b> Doppelklick auf den Pfad</li>
+<li><b>Eigenschaften des Ortes verändern:</b> Rechtsklick</li></ul>';
+$string['learningmap'] = 'Lernlandkarte';
+$string['learningmap:addinstance'] = 'Lernlandkarte hinzufügen';
+$string['learningmap:view'] = 'Lernlandkarte anzeigen';
+$string['modulename'] = 'Lernlandkarte';
+$string['modulename_help'] = 'Lernlandkarten erlauben es, die Aktivitäten in einem Kurs als durch Pfade verbundene Orte auf einer Landkarte darzustellen. Einzelne Orte sind Start-Orte und werden zu Beginn dargestellt. Andere Orte und Pfade werden angezeigt, sobald die Aktivitäten der verbundenen Orte abgeschlossen sind.';
+$string['modulenameplural'] = 'Lernlandkarten';
+$string['name'] = 'Titel der Lernlandkarte';
+$string['name_help'] = 'Der Titel der Lernlandkarte wird nur angezeigt, wenn "Karte auf Kursseite anzeigen" nicht aktiviert ist.';
+$string['nocompletion'] = 'Keine automatischer Abschluss über die Karte';
+$string['nocompletionenabled'] = 'Nicht vergügbar, da der Aktivitätsabschluss nicht aktiviert ist';
+$string['paths'] = 'Pfade';
+$string['places'] = 'Orte';
+$string['pluginadministration'] = 'Lernlandkarte Administration';
+$string['pluginname'] = 'Lernlandkarte';
+$string['privacy:metadata'] = 'Dieses Plugin verarbeitet keine personenbezogenen Daten.';
+$string['showdescription'] = 'Karte auf Kursseite anzeigen';
+$string['showdescription_help'] = 'Wenn diese Option aktiviert ist, wird die Lernlandkarte auf der Kursseite angezeigt. Ist sie deaktiviert, wird die Lernlandkarte auf einer separaten Seite dargestellt.';
+$string['startingplace'] = 'Start-Ort';
+$string['svgcode'] = 'SVG Code';
+$string['targetplace'] = 'Ziel-Ort';
+$string['usecasehelp'] = 'Wie verwende ich Lernlandkarten?';
+$string['usecaselink'] = 'Link zu einer Seite, die den Einsatz der Lernlandkarte erklärt';
+$string['usecheckmark'] = 'Haken für abgeschlossene Orte';
+$string['visited'] = 'Besucht';
