6af40e6c219f5ec0721173e7c4c3c8e5c5e89118
Stefan Hanauska <stefan.hanauska@csg-in.de>
2022-03-31 20:51:31 +0200

    MBS-6030: Bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_learningmap';
-$plugin->release = '0.4.2';
-$plugin->version = 2022020301;
+$plugin->release = '0.4.3';
+$plugin->version = 2022033101;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_BETA;
