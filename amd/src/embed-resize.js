dbfa7a27e154319e39e4b333af13ef4fa80d1f18
Stefan Hanauska <stefan.hanauska@csg-in.de>
2023-01-16 07:50:51 +0100

    MBS-7281: Add aspect ratio to resizer

--- amd/src/embed-resize.js
+++ amd/src/embed-resize.js
@@ -3,8 +3,10 @@ export const init = (iframeid) => {
      * Resize the learningmap iframe to show all of the content.
      */
     function resizeIframe() {
-        document.getElementById(iframeid).style.height =
-        document.getElementById(iframeid).contentWindow.document.body.querySelector('[role="main"]').scrollHeight + 'px';
+        let height = document.getElementById(iframeid).contentWindow.document.body.querySelector('[role="main"]').scrollHeight;
+        let width = document.getElementById(iframeid).contentWindow.document.body.querySelector('[role="main"]').scrollWidth;
+        document.getElementById(iframeid).style.height = height + 'px';
+        document.getElementById(iframeid).style.aspectRatio = parseInt(width) + '/' + parseInt(height);
     }
     let iframe = document.getElementById(iframeid);
     iframe.addEventListener('load', resizeIframe);
