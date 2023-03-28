554ea77833d233a110c5fd37305c9abf5c0f3c88
Stefan Hanauska <stefan.hanauska@csg-in.de>
2023-03-28 06:47:00 +0200

    Update classes/external/get_learningmap.php
    Co-authored-by: PhMemmel <65113153+PhMemmel@users.noreply.github.com>

--- classes/external/get_learningmap.php
+++ classes/external/get_learningmap.php
@@ -25,8 +25,8 @@
 
 namespace mod_learningmap\external;
 
+// Next 2 lines can be removed as soon as we inherit from \core_external\external_api instead of old \external_api.
 defined('MOODLE_INTERNAL') || die();
-
 require_once($CFG->dirroot . '/lib/externallib.php');
 
 use coding_exception;
