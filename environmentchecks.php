d98adf1934a9515dd6ae167aca88e7ddc062f4a7
Stefan Hanauska <stefan.hanauska@csg-in.de>
2023-06-04 11:03:12 +0200

    MBS-7747: Add environment check for FreeType

---
+++ environmentchecks.php
@@ -0,0 +1,34 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Environment checks for mod_learningmap
+ *
+ * @package     mod_learningmap
+ * @copyright   2021-2023, ISB Bayern
+ * @author      Stefan Hanauska <stefan.hanauska@csg-in.de>
+ * @license     https://www.gnu.org/licenses/agpl-3.0.html GNU AGPL v3 or later
+ */
+
+/**
+ * Detects whether FreeType extension for GD is installed
+ *
+ * @param object $result
+ * @return void
+ */
+function mod_learningmap_check_freetype($result) {
+    $result->setStatus(gd_info()['FreeType Support']);
+}
