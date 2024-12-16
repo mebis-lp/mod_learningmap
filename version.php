e9a101eac9ddd925346463694d67f510b69e2dfd
Stefan Hanauska <stefan.hanauska@csg-in.de>
2024-12-16 09:32:27 +0100

    Bump version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_learningmap';
-$plugin->release = '0.9.8.1';
-$plugin->version = 2024091601;
+$plugin->release = '0.9.8.2';
+$plugin->version = 2024121601;
 $plugin->requires = 2020061500;
-$plugin->supported = [401, 404];
+$plugin->supported = [401, 405];
 $plugin->maturity = MATURITY_STABLE;
